e88dc2a2b628910faa83b2595d70e7f9e87a9ffb
jselmani <jselmanovski1@myseneca.ca>
2020-03-29 16:43:15 -0400

    Completed Magic 8 Ball application

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey.shade900,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blueGrey.shade900,
+          ),
+          body: EightBallPage(),
+        ),
       ),
     );
+
+class EightBallPage extends StatefulWidget {
+  @override
+  _EightBallPageState createState() => _EightBallPageState();
+}
+
+class _EightBallPageState extends State<EightBallPage> {
+  int eightBallVersion = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            eightBallVersion = Random().nextInt(5) + 1;
+          });
+        },
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Image.asset('images/ball$eightBallVersion.png'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
